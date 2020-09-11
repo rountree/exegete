57e423c3c8665cf65a8033a6bd65b165a0d157ed
Barry Rountree <rountree@llnl.gov>
2020-09-11 22:17:55 +0000

    Able to count packages.

--- exegete.c
+++ exegete.c
@@ -2,10 +2,32 @@
 
 #include <linux/init.h>
 #include <linux/module.h>
+#include <linux/cpumask.h>	// for_each_online_cpu()
+#include <linux/topology.h>	// topology_physical_package_id()
+
+/* Topology information is scattered among:
+ * 	Documentation/x86/topology.rst
+ * 	Documentation/admin-guide/cputopology.rst
+ */
+
+static int get_physical_package_count(void){
+
+	// Stolen from drivers/idle/intel_idle.c
+	int cpu, package_num, num_sockets = 1;
+
+	for_each_online_cpu(cpu) {
+		package_num = topology_physical_package_id(cpu);
+		if (package_num + 1 > num_sockets) {
+			num_sockets = package_num + 1;
+		}
+	}
+	return num_sockets;
+}
 
 static int __init exegete_init(void)
 {
 	printk( KERN_INFO "Module exegete loaded successfully.\n");
+	printk( KERN_INFO "Number of packages=%d.\n", get_physical_package_count() );
 	return 0;
 }
 
