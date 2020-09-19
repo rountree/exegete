2d5fea0148cebc7cafe585bf46d581dbe2b996e7
Barry Rountree <rountree@llnl.gov>
2020-09-19 00:44:42 +0000

    Failed.

--- test.c
+++ test.c
@@ -1,20 +1,45 @@
+#include <stdint.h>
+#include <stdbool.h>
+#include <inttypes.h>
+#include <stdio.h>
+
 struct bitfield{
-	char *name;
-	uint8_t idx;
 	uint8_t hi;
 	uint8_t lo;
+	char *name;
 }bitfield;
 
 struct msr{
 	uint32_t addr;
 	char *name;
-	uint8_t num_bitfields;
 	struct bitfield bitfield[];
 };
 
-static struct msr foo={
-	{ .addr=0x610, .name="MSR_PKG_POWER_LIMIT", .num_bitfields=9, .bitfield={
-		{ .name="lock", 
+static struct msr foo[2]={
+	{ 0x610, "MSR_PKG_POWER_LIMIT", .bitfield= 
+						{  63, 63, "lock" },
+						{  62, 56, "reserved" },
+						{  55, 49, "window2" },
+						{  48, 48, "clamp2" },
+						{  47, 47, "enable2" },
+						{  46, 32, "limit2" },
+						{  31, 24, "reserved" },
+						{  23, 17, "window1" },
+						{  16, 16, "clamp1" },
+						{  15, 15, "enable1" },
+						{  14,  0, "limit1" }
+	},
+	{ 0x611, "MSR_PKG_ENERGY_STATUS", .bitfield= 
+						{  63, 32, "reserved" },
+						{  31,  0, "joules" }
+	}
+};
+
+
+int main(){
+	fprintf( stdout, "%s\n", foo[1].bitfield[1].name );		
+	return 0;
+}
 													     
 
 
